06ceb550a6f48177821a3bcb931c606511cb42af
Luke Gilbert <lukegilbert.416@gmail.com>
2025-06-16 22:05:05 +0100

    initial development on code metrics implmentation

---
+++ src/code_metrics/code_metrics.py
@@ -0,0 +1,31 @@
+import ast
+
+class CodeMetricsVisitor(ast.NodeVisitor):
+    
+    def __init__(self):
+        self.cyclomatic_complexity = 1
+        self.halstead_vocab = 0
+        self.halstead_length = 0
+        self.halstead_volume = 0
+        self.halstead_difficulty = 0
+        self.halstead_effort = 0
+        self.cognitive_complexity = 0
+        self.maintainability_index = 0
+        self.lines_of_code = 0
+
+    def visit(self, node):
+        print(f"Visiting: {node}")
+        self.calculate_cyclomatic(node)
+        self.generic_visit(node)
+
+    def calculate_cyclomatic(self, node: ast.AST):
+        self.decision_points = (ast.If, ast.For, ast.AsyncFor, ast.While, ast.With, ast.AsyncWith, ast.ExceptHandler, ast.Assert, ast.IfExp, ast.comprehension, ast.BoolOp)
+        if isinstance(node, self.decision_points):
+            self.cyclomatic_complexity += 1
+        print(f"Cyclomatic Complexity: {self.cyclomatic_complexity}")
+        
+
+with open('src/code_smells.py', 'r') as f:
+    source_code = f.read()
+
+CodeMetricsVisitor().visit(ast.parse(source_code))
--- src/code_smells.py
+++ src/code_smells.py
@@ -1,26 +1,7 @@
-import math
-
-from math import *
-
-global greeting
-
-def open_file_bad():
-    file = open('code_smells.py', 'r')
-    code = file.read()
-    print(code)
-    
-def open_file_good():
-    with open ('code_smells.py','r') as file:
-        code = file.read()
-        print(code)
-
-def bad_bool():
-    var = True
-    if var == True:
-        print("Greeting")
-
-def good_bool():
-    var = True
-    if var:
-        print("Hello")
-
+def if_elif_else(a):
+    if a > 10:
+        print("high")
+    elif a > 5:
+        print("medium")
+    else:
+        print("low")
